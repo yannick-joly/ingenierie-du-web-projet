b518829670a4d0cb202187a2bfeec3e5d3db9105
Loïc Le Goff <dev.legoff@gmail.com>
2025-05-11 11:50:06 +0200

    update front template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ function App() {
       </div>
 
       <div className="center">
+        {/* Remplacé Next/Image par une img standard */}
         <img
           className="logo"
           src={croiseur}
