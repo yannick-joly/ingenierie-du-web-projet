2dc8fd0d533b9119350e78209e2c2507180ce43c
Yannick Joly <yannick.joly@cpe.fr>
2026-05-07 10:46:10 +0200

    fix: wrong user names in SQL

--- fragments/data/init.sql
+++ fragments/data/init.sql
@@ -5,5 +5,7 @@ CREATE TABLE
         password_hash text
     );
 
-INSERT INTO users (id, name, password_hash) VALUES (1, "bean2", "$2y$10$OPOow5nX8acOM..8TgASnes30pjsP0NPjoCNBRReXHFu8nz2gxhhi");
-INSERT INTO users (id, name, password_hash) VALUES (2, "elfo2", "$2y$10$UMImr1tF2f4F6jt05XiNO.KoeIEUAoujZH30F9hWgNbQ368Lu0Dte");
+-- bean:Beer4Life
+INSERT INTO users (id, name, password_hash) VALUES (1, "bean", "$2y$10$OPOow5nX8acOM..8TgASnes30pjsP0NPjoCNBRReXHFu8nz2gxhhi");
+-- elfo:BumBum
+INSERT INTO users (id, name, password_hash) VALUES (2, "elfo", "$2y$10$UMImr1tF2f4F6jt05XiNO.KoeIEUAoujZH30F9hWgNbQ368Lu0Dte");
